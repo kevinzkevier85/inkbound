8d5d7d36937954c0c8c0e13ed605748de558d6fb
kevinzkevier85 <kevinzkevier85@gmail.com>
2026-05-02 03:39:03 +0700

    Delete inkbound_v05 directory

--- inkbound_v05/js/utils/random.js
+++
@@ -1,3 +0,0 @@
-export const rand = (a, b) => Math.floor(Math.random() * (b - a + 1)) + a;
-export const pick = arr => arr[Math.floor(Math.random() * arr.length)];
-export const clamp = (v, lo, hi) => Math.max(lo, Math.min(hi, v));
